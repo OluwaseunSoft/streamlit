03b38c271687ba71593d3c38b3af49b27a9a6b12
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2024-04-09 13:43:25 +0100

    output on streamlit

---
+++ st_write.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+
+#st.write()
+
+st.write('Well done Seun')
+
+l1= [1, 2, 3]
+
+st.write(l1)
+
+l2 = list('abc')
+d1 = dict(zip(l1, l2))
+st.write(d1)
+
+#magic
+
+'Displaying using magic :smile:'
