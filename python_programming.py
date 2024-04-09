4b9ff42a4484ed0212a55f58c0d4d7f7a61c80f1
OluwaseunSoft <Oluwaseunazure@yahoo.com>
2024-04-09 21:21:38 +0100

    working with json

--- python_programming.py
+++ python_programming.py
@@ -1,11 +1,20 @@
+import json
+
+#Working with JSON
+
+friends = {'Dan':[20, 'London', 3234342], 'Maria': [25, 'Madrid', 45544212]}
+
+with open('friends.json', 'w') as f:
+    json.dump(friends, f)
+
 # While and Continue
 
-x = 12
-while x < 100:
-    x = x + 1
-    if x % 13 != 0:
-        continue
-    #print(x)
+# x = 12
+# while x < 100:
+#     x = x + 1
+#     if x % 13 != 0:
+#         continue
+#     #print(x)
 
 # While and Continue
 # while True:
@@ -27,15 +36,36 @@ while x < 100:
 #     print(f'{x}')
 
 #List Comprehension
-clicks = [10, 6, 4, 5]
-doubled_list = list()
-for c in clicks:
-    doubled_list.append(c * 2)
-print(doubled_list)
+# clicks = [10, 6, 4, 5]
+# doubled_list = list()
+# for c in clicks:
+#     doubled_list.append(c * 2)
+# print(doubled_list)
 
-doubled_numbers = [c * 2 for c in clicks]
-print(doubled_numbers)
+# doubled_numbers = [c * 2 for c in clicks]
+# print(doubled_numbers)
 
-name = 'Andrei'
-l1 = [char for char in name]
-print(l1)
+# name = 'Andrei'
+# l1 = [char for char in name]
+# print(l1)
+
+#Dictionaries
+
+# person = {'name': 'John', 'age': 30, (1, 2, 3): 100}
+# #print(person[(1, 2, 3)])
+
+# person['location'] = 'Africa'
+# #print(person)
+
+# a = person['location']
+# #print(a)
+
+# germany = {
+#     'cities' : ['Hamburg', 'Berlin', 'Munich'],
+#     'info': {'population': 566_779, 'people': ['Einstein', 'Bach', 'Gauss']}
+# }
+
+# print(germany['info']['people'][1])
+
+
+
